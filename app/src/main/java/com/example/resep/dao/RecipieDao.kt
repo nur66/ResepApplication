b8d025d03cd25f4edca3910a2f45115ac36f845d
Nurdev <nuriswanto66@gmail.com>
2023-04-02 02:32:09 -0700

    Integrated Database

---
+++ app/src/main/java/com/example/resep/dao/RecipieDao.kt
@@ -0,0 +1,17 @@
+package com.example.resep.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.resep.entities.Recipies
+
+@Dao
+interface RecipieDao {
+
+    @get:Query("SELECT * FROM recipes ORDER BY id DESC")
+    val allRecipes: List<Recipies>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertRecipe(recipies: Recipies)
+}
