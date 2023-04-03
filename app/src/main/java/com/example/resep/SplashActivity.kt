970def8a605a517ef09dc200bf15ceb263084c47
Nur <nuriswanto66@gmail.com>
2023-04-03 12:08:09 +0700

    Home Activity

--- app/src/main/java/com/example/resep/SplashActivity.kt
+++ app/src/main/java/com/example/resep/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.resep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_splash)
+
+        val btnGetStarted : Button = findViewById(R.id.btnGetStarted)
+        btnGetStarted.setOnClickListener {
+            var intent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
